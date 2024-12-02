9d83c15d83754f56b891d56f182022996b208e6e
joschonarth <joschonarth@gmail.com>
2024-12-02 19:12:54 -0300

    feat: define initial routes

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/payments/pix', methods=['POST'])
+def create_payment_pix():
+    return jsonify({"message": "The payment has benn created"})
+
+@app.route('/payments/pix/confirmation', methods=['POST'])
+def pix_confirmation():
+    return jsonify({"message": "The payment has benn confirmed"})
+
+@app.route('/payments/pix/<int:payment_id>', methods=['GET'])
+def payment_pix_page(payment_id):
+    return 'Pix Payment'
+
+if __name__ == '__main__':
+    app.run(debug=True)
