12b3bdf52f3a54f293295c15fa8ae3c6aa31c28c
joschonarth <joschonarth@gmail.com>
2024-12-03 19:20:34 -0300

    feat: implement websocket

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
 from payments.pix import Pix
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'secret_key_payment_project'
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
@@ -52,5 +54,9 @@ def payment_pix_page(payment_id):
         qr_code=payment.qr_code
     )
 
+@socketio.on('connect')
+def handle_connect():
+    print("Client connected to the server")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
